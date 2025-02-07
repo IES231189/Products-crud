f1f916255c00bb3d54df2ca6e01479fa77aa31c5
231189alo <231189@ids.upchiapas.edu.mx>
2025-02-06 18:13:42 -0600

    Modelo para productos

---
+++ src/app/core/entities/Producto/productos.ts
@@ -0,0 +1,9 @@
+export interface Producto {
+  Id_producto: number;
+  Nombre: string;
+  Descripcion: string;
+  Precio: number;
+  Stock: number;
+  Id_categoria: number;
+  Imagen_url: string;
+}
