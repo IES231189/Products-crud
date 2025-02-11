01bb1f33a58a8193bd3925a84336d8dd1489b237
231189alo <231189@ids.upchiapas.edu.mx>
2025-02-10 22:30:10 -0600

    routing: rutas generales

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AppModule } from './app.module';
+import { MostrarOfertasComponent } from './presentation/feature/ofertas/mostrar-ofertas/mostrar-ofertas.component';
+import { OptenerProductosComponent } from './presentation/feature/Productos/Mostrar-productos/optener-productos.component';
+import { ObtenerProductosService } from './use-cases/Productos/obtener-productos.service';
 
-const routes: Routes = [];
-
+const routes: Routes = [
+  { path: '', redirectTo: 'producto', pathMatch: 'full' },
+  { path: 'producto', component:OptenerProductosComponent },
+  { path: 'ofertas', component: MostrarOfertasComponent },
+  { path: '**', redirectTo: 'producto' }                  
+];
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
